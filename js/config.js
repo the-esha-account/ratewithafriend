47a39f2aa94ff7f125f93186698295ad7c01cda5
the-esha-account <esha.kanakapura@gmail.com>
2025-02-05 21:28:04 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://adfvhgakefvtfnftjwdd.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZnZoZ2FrZWZ2dGZuZnRqd2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4MTk0NTAsImV4cCI6MjA1NDM5NTQ1MH0.KrBKNwekmW8Ov-0vlFS4h-FaSt9Y0gjoXwXB04UVczg';
-const IMGUR_CLIENT_ID = 'your-imgur-client-id';
+const IMGUR_CLIENT_ID = '8db0a94a56a0071';
 
 export { SUPABASE_URL, SUPABASE_KEY, IMGUR_CLIENT_ID };
